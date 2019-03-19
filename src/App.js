3e20312837bdcdf0253c7fad1d4d4f12ea196f58
Lucas Grönborg <lucas94gronborg@gmail.com>
2019-03-19 10:49:06 +0100

    Pie graph reacts to range input
    The pie graph dynamically changes based on the range input

--- src/App.js
+++ src/App.js
@@ -4,18 +4,6 @@ import NumericInput from 'react-numeric-input';
 import { VictoryPie, VictoryLabel } from 'victory';
 
 // 1. INPUT CURRENT CO2e
-function NumberInputForm (props) {
-  return (
-    <form onSubmit={props.handleSubmit}>
-      <label>
-        <NumericInput className="NumberInput" strict={true} min={0} precision={1} value={props.value} onChange={props.handleChange}/>
-        ton
-      </label>
-      <input type="submit" value="Nästa steg" />
-    </form>
-  );
-}
-
 class InputPage extends Component {
   constructor(props) {
     super(props);
@@ -30,94 +18,89 @@ class InputPage extends Component {
   }
 
   handleSubmit(event) {
-    alert('Dina växthusgasutsläpp på ett år är ' + this.state.tempCurrentCO2e + ' ton COe');
-    event.preventDefault();
+    this.props.confirm(this.state.tempCurrentCO2e);
   }
 
   render() {
     return (
       <div>
-        <div>Fyll i ditt totala växthusgasutsläpp för ett år nedan</div>
+        <div>Fyll i nedan ditt totala växthusgasutsläpp för ett år</div>
         <NumberInputForm value={this.state.tempCurrentCO2e} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
       </div>
     );
   }
 }
 
-
-// 2. SETTING CO2e GOAL BUDGET LIMIT
-function RangeInputForm(props) {
+function NumberInputForm (props) {
   return (
     <form onSubmit={props.handleSubmit}>
-      <input type="range" min="0" max={props.max} step="0.1" value={props.startValue} onChange={props.handleChange}/>
+      <label>
+        <NumericInput className="NumberInput" strict={true} min={0} precision={1} value={props.value} onChange={props.handleChange}/>
+        ton
+      </label>
       <br/>
-      <input type="submit" value="Nästa steg"/>
+      <input type="submit" value="Nästa steg" />
     </form>
   );
 }
 
-class BudgetGraph extends Component {
-  constructor(props) {
-    super(props);
-    this.upperLimitRadius = 100;
-    this.state = {
-      goalRadius: this.props.goalBudget/this.props.upperLimit*this.upperLimitRadius,
-      exampleRadius: this.props.exampleAverage/this.props.upperLimit*this.upperLimitRadius
-    };
-    console.log("Start goal value: " + this.state.goalRadius);
-  }
 
-  render() {
-    return (
-      <svg viewBox="0 0 220 220">
-        <VictoryPie
-          standalone={false}
-          width={220} height={220}
-          data={[{x: " ", y: 1}]}
-          radius={this.state.goalRadius}
-          innerRadius={this.state.goalRadius-20}
-          colorScale={["green"]}
-        />
-        <circle cx="110" cy="110" r={this.state.exampleRadius} fill="none" stroke="black" strokeWidth={3} strokeDasharray="6"/>
-        <circle cx="110" cy="110" r={this.upperLimitRadius} fill="none" stroke="red" strokeWidth={3}/>
-        <VictoryLabel
-          textAnchor="middle" verticalAnchor="middle"
-          x={110} y={110}
-          style={{fontSize: 20}}
-          text="CO2e"
-        />
-      </svg>
-    );
-  }
-}
-
+// 2. SETTING CO2e GOAL BUDGET LIMIT
 class SetBudgetPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {tempGoalCO2e: this.props.currentCO2e/2};
+    this.upperLimitRadius = 100;
+    this.exampleAverage = this.props.currentCO2e/2; // Temporary
+    this.exampleRadius = this.calculateRadius(this.exampleAverage);
 
+    this.state = {
+      tempGoalCO2e: this.props.currentCO2e/2,
+      goalRadius: this.calculateRadius(this.props.currentCO2e/2)
+    };
+
+    // this.calculateRadius = this.calculateRadius.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  calculateRadius(CO2eValue) {
+    console.log("I'm working!");
+    return CO2eValue/this.props.currentCO2e*this.upperLimitRadius;
+  }
+
   handleChange(event) {
-    this.setState({tempGoalCO2e: event.target.value});
+    this.setState({
+      tempGoalCO2e: event.target.value,
+      goalRadius: this.calculateRadius(event.target.value)
+    });
   }
 
   handleSubmit(event) {
-    alert('Du har satt din målbudget på ' + this.state.tempGoalCO2e + ' ton COe');
-    event.preventDefault();
+    this.props.confirm(this.state.tempGoalCO2e);
   }
 
   render() {
     return (
       <div>
         <div>Sätt taket för din målbudget</div>
-        <BudgetGraph
-          goalBudget={this.state.tempGoalCO2e}
-          upperLimit={this.props.currentCO2e}
-          exampleAverage={this.props.currentCO2e/2}
-        />
+        <svg viewBox="0 0 220 220">
+          <VictoryPie
+            standalone={false}
+            width={220} height={220}
+            data={[{x: " ", y: 1}]}
+            radius={this.state.goalRadius}
+            innerRadius={this.state.goalRadius-20}
+            colorScale={["green"]}
+          />
+          <circle cx="110" cy="110" r={this.exampleRadius} fill="none" stroke="black" strokeWidth={3} strokeDasharray="6"/>
+          <circle cx="110" cy="110" r={this.upperLimitRadius} fill="none" stroke="red" strokeWidth={3}/>
+          <VictoryLabel
+            textAnchor="middle" verticalAnchor="middle"
+            x={110} y={110}
+            style={{fontSize: 20}}
+            text="CO2e"
+          />
+        </svg>
         <div>{this.state.tempGoalCO2e}</div>
         <RangeInputForm
           max={this.props.currentCO2e}
@@ -130,6 +113,49 @@ class SetBudgetPage extends Component {
   }
 }
 
+function RangeInputForm(props) {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <input type="range" min="0" max={props.max} step="0.1" value={props.startValue} onChange={props.handleChange}/>
+      <br/>
+      <input type="submit" value="Tillbaka"/>
+    </form>
+  );
+}
+
+class BudgetGraph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      externalMutations: this.props.externalMutations,
+    };
+  }
+
+  render() {
+    return (
+      <svg viewBox="0 0 220 220">
+        <VictoryPie
+          externalEventMutations={this.state.externalMutations}
+          standalone={false}
+          width={220} height={220}
+          data={[{x: " ", y: 1}]}
+          radius={this.props.goalRadius}
+          innerRadius={this.props.goalRadius-20}
+          colorScale={["green"]}
+        />
+        <circle cx="110" cy="110" r={this.props.exampleRadius} fill="none" stroke="black" strokeWidth={3} strokeDasharray="6"/>
+        <circle cx="110" cy="110" r={this.props.upperLimitRadius} fill="none" stroke="red" strokeWidth={3}/>
+        <VictoryLabel
+          textAnchor="middle" verticalAnchor="middle"
+          x={110} y={110}
+          style={{fontSize: 20}}
+          text="CO2e"
+        />
+      </svg>
+    );
+  }
+}
+
 
 // THE WHOLE WEB APP
 class App extends Component {
@@ -138,14 +164,45 @@ class App extends Component {
     this.state = {
       phase: 1,
       currentCO2e: 11,
-      targetCO2e: null,
+      budgetLimit: null,
     };
   }
 
+  confirmCurrentEmissions(value) {
+    this.setState({
+      currentCO2e: value,
+      phase: 2
+    });
+  }
+
+  confirmBudgetLimit(value) {
+    this.setState({
+      budgetLimit: value,
+      phase: 1
+    });
+  }
+
   render() {
+    let mainContent;
+    switch (this.state.phase) {
+      case 1:
+        mainContent = <InputPage confirm={(v)=>this.confirmCurrentEmissions(v)}/>
+        break;
+      case 2:
+        mainContent = <SetBudgetPage
+                        currentCO2e={this.state.currentCO2e}
+                        confirm={(v)=>this.confirmBudgetLimit(v)}
+                      />
+        break;
+      case 3:
+        break;
+      default:
+        break;
+    }
+
     return (
       <div className="App">
-        <SetBudgetPage currentCO2e={this.state.currentCO2e}/>
+        {mainContent}
       </div>
     );
   }
