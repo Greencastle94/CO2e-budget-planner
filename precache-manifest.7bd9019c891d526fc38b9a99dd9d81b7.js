0dd0e8e02435875f38d0c72d725e74d483ed2fde
Lucas Grönborg <lucas94gronborg@gmail.com>
2019-03-27 15:23:48 +0100

    Updates

---
+++ precache-manifest.7bd9019c891d526fc38b9a99dd9d81b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "28862eea37d5bf39077d",
+    "url": "/CO2e-budget-planner/static/css/main.7225d644.chunk.css"
+  },
+  {
+    "revision": "28862eea37d5bf39077d",
+    "url": "/CO2e-budget-planner/static/js/main.061df0a2.chunk.js"
+  },
+  {
+    "revision": "67d9cde898383a054130",
+    "url": "/CO2e-budget-planner/static/js/runtime~main.bd161a21.js"
+  },
+  {
+    "revision": "f1e790ec928525db178e",
+    "url": "/CO2e-budget-planner/static/js/2.92e334c6.chunk.js"
+  },
+  {
+    "revision": "1aef463b4c2f9800341e84d825aa0fa5",
+    "url": "/CO2e-budget-planner/index.html"
+  }
+];
